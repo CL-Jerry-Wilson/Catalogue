1a4cdba0064c67f39cbdeaa5a4be3faf6c22050d
CL-Jerry-Wilson <jerry.wilson@cirruslabs.io>
2024-07-01 01:22:44 -0400

    Update jest.config.ts
    Making sure Jest for TypeScript is configured

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
 import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   projects: await getJestProjectsAsync(),
+  testMatch: ['**/tests/**/*.test.ts'],
 });
