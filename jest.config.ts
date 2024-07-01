23e795050b77935e423b501d5e97751bd5175701
CL-Jerry-Wilson <jerry.wilson@cirruslabs.io>
2024-07-01 02:17:23 -0400

    Update jest.config.ts
    additional configs

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,10 @@ import { getJestProjectsAsync } from '@nx/jest';
 export default async () => ({
   preset: 'ts-jest',
   testEnvironment: 'node',
-  projects: await getJestProjectsAsync(),
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/tests/**/*.test.ts'],
+  projects: await getJestProjectsAsync(),
 });
